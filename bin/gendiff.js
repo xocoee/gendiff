77ef887f9cb22e585a679500e250109fec14ff26
xocoee <xocoee@gmail.com>
2024-02-16 23:20:29 +0200

    executed 2 step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,6 +6,8 @@ const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0')
+  .version('0.0.1')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format')
 
   .parse();
