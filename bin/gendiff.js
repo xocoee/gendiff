7e104a506c730f79f24b52f1e02797532aa65daf
xocoee <xocoee@gmail.com>
2024-02-18 22:13:29 +0200

    add 'stylish' and fix version

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,9 +7,9 @@ const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1')
+  .version('1.0.0')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .action((filepath1, filepath2) => {
     console.log(genDiff(filepath1, filepath2, program.opts().format));
   })
