c61d7a2b441c4b274959abcfbd3e9b4404c154b4
xocoee <xocoee@gmail.com>
2024-02-20 12:58:38 +0200

    fix mentors 1

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,16 +1,6 @@
 import _ from 'lodash';
 
 const getSpaces = (depth) => '    '.repeat(depth);
-
-const marks = {
-  openBracket: '{',
-  closeBracket: '}',
-  removed: '- ',
-  added: '+ ',
-  unmodified: '  ',
-  nested: '  ',
-};
-
 const getCloseBracket = (depth) => `${getSpaces(depth - 1)}}`;
 
 const stringify = (node, depth = 1) => {
@@ -31,21 +21,20 @@ const stylish = (diffTree) => {
       const {
         key, status, value, previous, current, children,
       } = prop;
-      const stringStarter = `${indent}${status === 'removed' ? '- ' : '+ '}${key}`;
       switch (status) {
         case 'removed':
-          return [...acc, `${stringStarter}: ${stringify(value, depth + 1)}`];
+          return [...acc, `${indent}- ${key}: ${stringify(value, depth + 1)}`];
         case 'added':
-          return [...acc, `${stringStarter}: ${stringify(value, depth + 1)}`];
+          return [...acc, `${indent}+ ${key}: ${stringify(value, depth + 1)}`];
         case 'updated':
           return [
             ...acc,
             `${indent}- ${key}: ${stringify(previous, depth + 1)}\n${indent}+ ${key}: ${stringify(current, depth + 1)}`,
           ];
         case 'nested':
-          return [...acc, `${stringStarter}: ${iter(children, depth + 1)}`];
+          return [...acc, `${indent}  ${key}: ${iter(children, depth + 1)}`];
         default:
-          return [...acc, `${stringStarter}: ${stringify(value, depth + 1)}`];
+          return [...acc, `${indent}  ${key}: ${stringify(value, depth + 1)}`];
       }
     }, []);
     return ['{', ...strings, getCloseBracket(depth)].join('\n');
