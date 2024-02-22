a062b18b7a86cf702f427fa7999d04a35f7970b2
xocoee <xocoee@gmail.com>
2024-02-22 13:56:52 +0200

    test commit

--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@ export default (filePath1, filePath2, format = 'stylish') => {
   const path2 = getAbsolutePath(filePath2);
   const extname1 = getExtension(path1);
   const extname2 = getExtension(path2);
-  const data1 = readFileSync(path1, extname1);
-  const data2 = readFileSync(path2, extname2);
+  const data1 = readFileSync(path1, 'utf8');
+  const data2 = readFileSync(path2, 'utf8');
   const parsedData1 = parse(data1, extname1);
   const parsedData2 = parse(data2, extname2);
   const diffTree = buildDiffTree(parsedData1, parsedData2);
